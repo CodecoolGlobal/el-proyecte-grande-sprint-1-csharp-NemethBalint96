6cd50669504a837f3d0c47b7b62b1f51f93e3e2e
NemethBalint96 <nemeth.balint1996@gmail.com>
2022-07-18 15:36:05 +0200

    add room modell

---
+++ ElProyecteGrande/Models/Room.cs
@@ -0,0 +1,25 @@
+﻿namespace ElProyecteGrande.Models;
+
+public enum RoomType
+{
+    Apartman = 4,
+    Standard = 2,
+    Superior = 3
+}
+
+public enum RoomStatus
+{
+    Active,
+    Inactive
+}
+
+public class Room
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public RoomType RoomType { get; set; }
+    public RoomStatus RoomStatus { get; set; }
+    public int Floor { get; set; }
+    public string Comment { get; set; }
+}
