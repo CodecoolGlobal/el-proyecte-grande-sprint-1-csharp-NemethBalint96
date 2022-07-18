dfd48c2bc5d6025bf8e3bd16e762746286eb2626
NemethBalint96 <nemeth.balint1996@gmail.com>
2022-07-18 15:35:48 +0200

    add booking modell

---
+++ ElProyecteGrande/Models/Booking.cs
@@ -0,0 +1,27 @@
+﻿namespace ElProyecteGrande.Models;
+
+public enum Status
+{
+    Confirmed,
+    Cancelled
+}
+
+public class Booking
+{
+    public Guid ID { get; set; }
+    public string BookersName { get; set; }
+    public List<Guest> Guests { get; set; }
+    public string Email { get; set; }
+    public Room Room { get; set; }
+    public int Nights { get; set; }
+    public int Adults { get; set; }
+    public int Children { get; set; }
+    public int Infants { get; set; }
+    public decimal Total { get; set; }
+    public string Country { get; set; }
+    public DateOnly ArrivalDate { get; set; }
+    public DateOnly DepartureDate { get; set; }
+    public Status Status { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime ModificationDate { get; set; }
+}
