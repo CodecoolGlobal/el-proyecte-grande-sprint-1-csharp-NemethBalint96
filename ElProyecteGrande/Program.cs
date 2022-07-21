6e23f69adecb15f7f32c66c892d2cd8cb94e5818
Kissbence29 <kissbence19@gmail.com>
2022-07-21 11:34:08 +0200

    Dataseed in progress

--- ElProyecteGrande/Program.cs
+++ ElProyecteGrande/Program.cs
@@ -1,4 +1,5 @@
 using ElProyecteGrande.Dao;
+using ElProyecteGrande.Data;
 using ElProyecteGrande.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,49 +18,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-void SetupInMemoryDatabase()
-{
-    BookingDaoMemory.GetInstance().Add(new Booking
-    {
-        ArrivalDate = DateTime.Now,
-        DepartureDate = DateTime.Parse("2022.07.23"),
-        BookersName = "Németh Bálint",
-        Country = "Hungary",
-        Email = "nemeth.balint1996@gmail.com",
-        Adults = 4,
-        Infants = 2,
-        Children = 3,
-        Guests = new List<Guest>{
-            new Guest(Age.Adult)
-            {
-                FullName = "Németh Bálint",
-                BirthDate = DateOnly.Parse("1996.01.01"),
-                BirthPlace = "Eger",
-                Email = "nemeth.balint1996@gmail.com",
-                Phone = "00000000",
-                Country = "Hungary",
-                City = "Füzesabony",
-                Address = "",
-                PostalCode = 3390,
-                Citizenship = "Hungary",
-            },
-            new Guest(Age.Infant)
-            {
-                BirthDate = DateOnly.Parse("2022.01.01"),
-            }
-        },
-        Room = new Room
-        {
-            Comment = "  ",
-            Floor = 1,
-            DoorNumber = 2,
-            Price = 20
-        },
-        Status = Status.Confirmed,
-    });
-}
 
-SetupInMemoryDatabase();
+Dataseed.SetupInMemoryDatabase(BookingDaoMemory.GetInstance());
+
 app.UseHttpsRedirection();
 
 app.UseSession();
