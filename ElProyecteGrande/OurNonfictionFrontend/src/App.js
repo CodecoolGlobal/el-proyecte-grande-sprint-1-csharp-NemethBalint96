e99edc1ed047bdcc8f8fd6a2d6a7d939d51281a2
NemethBalint96 <nemeth.balint1996@gmail.com>
2022-08-03 15:16:41 +0200

    navbar style

--- ElProyecteGrande/OurNonfictionFrontend/src/App.js
+++ ElProyecteGrande/OurNonfictionFrontend/src/App.js
@@ -1,18 +1,23 @@
 import { Link, Outlet } from "react-router-dom";
 
 const App = () => {
-
- return (
+  return (
     <>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/">Bookings</Link>{" "}          
-        <Link to="/guests">Guests</Link>{" "}
-      </nav>
+      <header>
+        <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+          <div className="container-fluid">
+            <a className="navbar-brand" href='/'>Our Nonfiction</a>
+              <ul className="navbar-nav flex-grow-1">
+                <li className="nav-item">
+                  <Link className="nav-link text-dark" to="/">Bookings</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link text-dark" to="/guests">Guests</Link>
+                </li>
+              </ul>
+          </div>
+        </nav>
+      </header>
       <Outlet/>
       <footer className="footer mt-auto py-3 bg-ligh border-top text-muted">
         <div className="container text-center">
