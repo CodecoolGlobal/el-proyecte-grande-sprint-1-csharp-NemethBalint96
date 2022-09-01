d3c6919c138a817c18542dbdb50844669e644379
NemethBalint96 <nemeth.balint1996@gmail.com>
2022-09-01 16:12:15 +0200

    save google user for userpage

--- ElProyecteGrande/OurNonfictionBackend/Models/Account.cs
+++ ElProyecteGrande/OurNonfictionBackend/Models/Account.cs
@@ -8,6 +8,6 @@ public class Account
     public long Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public string? Password { get; set; }
     public string Role { get; set; }
 }
