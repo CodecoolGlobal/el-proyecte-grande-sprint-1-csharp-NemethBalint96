2f0f96eba7e12ba0b0d73bbf066e63b627d1ec6f
Kiss Bence <kissbence19@gmail.com>
2022-09-12 19:35:13 +0200

    Refactoring test project, testing guest service

--- ElProyecteGrande/OurNonFictionTest/RoomServiceTest.cs
+++ ElProyecteGrande/OurNonFictionTest/RoomServiceTest.cs
@@ -1,44 +1,21 @@
 using ElProyecteGrande.Dal;
 using ElProyecteGrande.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using Newtonsoft.Json;
 using NSubstitute;
-using OurNonfictionBackend.Dal;
 using OurNonfictionBackend.Models;
 
 namespace OurNonFictionTest;
 
 public class RoomServiceTest
 {
-    private readonly DbContextOptions<NonfictionContext> _contextOptions;
+
     private RoomService _roomService;
     private NonfictionContext _context;
-    private GuestService _guestService;
-    public RoomServiceTest()
-    {
-        _contextOptions = new DbContextOptionsBuilder<NonfictionContext>()
-            .UseInMemoryDatabase("nonfiction")
-            .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
-            .Options;
-
-        using var context = new NonfictionContext(_contextOptions);
-        var bookingJson = File.ReadAllText(@"Data\newBooking.json");
-        var bookings = JsonConvert.DeserializeObject<List<Booking>>(bookingJson);
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
-
-        context.Bookings.AddRange(bookings);
-        context.Accounts.Add(new Account() { Email = "admin@admin.com", Username = "admin", Password = BCrypt.Net.BCrypt.HashPassword("admin"), Role = "Admin" });
-        context.SaveChanges();
-    }
-
     [SetUp]
     public void Setup()
     {
-        _roomService = Substitute.For<RoomService>(CreateContext());
-        _guestService = Substitute.For<GuestService>(CreateContext());
-        _context = CreateContext();
+        _roomService = Substitute.For<RoomService>(new InitDatabase().CreateContext());
+        _context = new InitDatabase().CreateContext();
     }
 
     [Test]
@@ -58,6 +35,13 @@ public class RoomServiceTest
         Assert.That(actual, Is.EqualTo(expected));
     }
 
+    [Test]
+    public void AddRoomToBookingReturnsTheSameRoom()
+    {
+        _roomService.AddRoomToBooking(3, 1);
+        var expected = 3;
+        var actual = _context.Bookings.Include(Booking => Booking.Room).First().Room!.Id;
+        Assert.That(actual, Is.EqualTo(expected));
     [Test]
     public void AddRoomToBookingReturnsTheSameRoom()
     {
@@ -120,6 +104,7 @@ public class RoomServiceTest
         Assert.That(actual, Is.EqualTo(expected));
     }
 
+    }
     [Test]
     public void GetAllNamedGuests_ReturnsGuestWhosNameIsNotAccompanyingGuest()
     {
@@ -127,6 +112,9 @@ public class RoomServiceTest
         var actual = _guestService.GetAllNamedGuests().Result.ToList().Count;
         Assert.That(actual, Is.EqualTo(expected));
     }
+
+
+};
 
     [Test]
     public async Task DeleteGuestFromBookingDecreaseTheNumberOfGuestsInBooking()
@@ -137,6 +125,5 @@ public class RoomServiceTest
         var actual = _context.Bookings.Include(booking => booking.Guests).First().Guests.Count;
         Assert.That(actual, Is.EqualTo(expected));
     }
-    NonfictionContext CreateContext() => new(_contextOptions);
 };
 
