b5fcf5cf16be1f8e6ba8cd805945c221a1b0d313
Kiss Bence <kissbence19@gmail.com>
2022-09-13 18:41:07 +0200

    changing newbooking.json

--- OurNonFictionTest/InitDatabase.cs
+++ OurNonFictionTest/InitDatabase.cs
@@ -17,12 +17,27 @@ public class InitDatabase
             .Options;
 
         using var context = new NonfictionContext(_contextOptions);
-        var bookingJson = File.ReadAllText(@"C:\Users\Kiss Bence\Desktop\Codecool_Advanced\el-proyecte-grande-sprint-1-csharp-NemethBalint96\OurNonfictionBackend\Data\newbooking.json");
-        var bookings = JsonConvert.DeserializeObject<List<Booking>>(bookingJson);
+        //var bookingJson = File.ReadAllText(@"Data\newBooking.json");
+        //var bookings = JsonConvert.DeserializeObject<List<Booking>>(bookingJson);
         context.Database.EnsureDeleted();
         context.Database.EnsureCreated();
-
-        context.Bookings.AddRange(bookings);
+        var booking = new Booking
+        {
+            BookersName = "Kiss Bence",
+            Guests = null,
+            Email = "kissbence19@gmail.com",
+            Room = null,
+            Adults = 0,
+            Children = 0,
+            Infants = 0,
+            Country = "Hun",
+            ArrivalDate = DateTime.Now,
+            DepartureDate = DateTime.Now.AddDays(1),
+            Status = Status.Confirmed,
+            Created = DateTime.Now,
+            ModificationDate = DateTime.Now
+        };
+        context.Bookings.Add(booking);
         context.Accounts.Add(new Account()
         {
             Email = "admin@admin.com",
