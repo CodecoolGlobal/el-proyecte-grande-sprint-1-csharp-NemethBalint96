813e780a8cf24ff895f94eb73211822df0f4d31e
Kiss Bence <kissbence19@gmail.com>
2022-09-14 20:14:16 +0200

    testing

--- OurNonfictionBackend/Program.cs
+++ OurNonfictionBackend/Program.cs
@@ -64,10 +64,11 @@ builder.Services.AddSingleton<IJWTAuthenticationManager>(new JWTAuthenticationMa
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
